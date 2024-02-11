3b72123b1ab0680e706414e217f83942ce318a0e
Ctheo <85225231+Christheoguipo@users.noreply.github.com>
2024-02-11 07:46:01 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,9 @@ const navLinkElements = document.querySelectorAll('.navigation-list__link');
 const sectionElements = document.querySelectorAll('section');
 
 let currentSection = 'home-section';
-const headerHeight = document.querySelector('.page-header').offsetHeight;
+const headerHeight = document.querySelector('.navbar__content').offsetHeight;
 
+// Highlight the menu items
 window.addEventListener('scroll', () => {
 
   sectionElements.forEach(sectionElement => {
@@ -14,8 +15,37 @@ window.addEventListener('scroll', () => {
 
   navLinkElements.forEach(navLinkElement => {
     if (navLinkElement.href.includes(currentSection)) {
-      document.querySelector('.active').classList.remove('active');
+      if (document.querySelector('.active')) {
+        document.querySelector('.active').classList.remove('active');
+      }
+
       navLinkElement.classList.add('active');
     }
+  });
+});
+
+
+const hamburgerElement = document.querySelector('.hamburger');
+const navigationListWrapperElement = document.querySelector('.navigation-list__wrapper');
+
+// Show or Hide the menu list once the Hamburger menu is clicked 
+hamburgerElement.addEventListener('click', () => {
+  navigationListWrapperElement.classList.toggle('navbar-show');
+});
+
+// Hide the menu list once a link is clicked on smaller devices
+navLinkElements.forEach(navLinkElement => {
+  navLinkElement.addEventListener('click', () => {
+    if (document.querySelector('.navbar-show')) {
+      navigationListWrapperElement.classList.remove('navbar-show');
+    }
+  });
+});
+
+// Imitates the hover effect on mobile devices
+const projectImgWrappers = document.querySelectorAll('.project-img-wrapper');
+projectImgWrappers.forEach(projectImgWrapper => {
+  projectImgWrapper.addEventListener('touchstart', function () {
+    // Leave blank
   });
 });
