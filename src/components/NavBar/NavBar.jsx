9aa23c4b24d75d41d1edc9a827e48a4809b02d70
Theo <christheo.guipo@gmail.com>
2025-06-12 23:52:12 +0200

    feat: change navbar styling.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
+import "./NavBar.css";
 
 function NavBar() {
   const [showMenu, setShowMenu] = useState(false);
@@ -16,33 +17,110 @@ function NavBar() {
   return (
     <div className="navbar">
       <div className="navbar-content">
-        <a href="#home-section">
-          <img
+        {/* <img
             src="images/christheo-guipo-logo.png"
             className="page-header-logo"
             alt="My personal logo, a text logo in uppercase letters of my full name"
-          />
-        </a>
-        <div>
+          /> */}
+        <div className="page-header-logo">
+          <img src="images/logo.jpg" className="logo" />
+        </div>
+        <div className="page-header-name">
+          <span>Christheo Guipo</span>
+          <span className="subtext">Software Developer</span>
+        </div>
+
+        {/* <div>
           <FontAwesomeIcon
             onClick={handleHamburgerMenuClick}
             icon={faBars}
             className="fa-xl hamburger"
           />
-        </div>
+        </div> */}
 
         <div
           className={`navigation-list-wrapper ${showMenu ? "navbar-show" : ""}`}
         >
-          <ul role="menubar" className="navigation-list">
+          <div className="navigation-list">
+            <a
+              href="#home-section"
+              role="menuitem"
+              className="navigation-list-link"
+              onClick={handleLinkClick}
+            >
+              <div className="navigation-list-item">
+                <img
+                  src="images/home-icon.svg"
+                  className="navigation-list-link-image"
+                />
+              </div>
+            </a>
+
+            <a
+              href="#about-section"
+              role="menuitem"
+              className="navigation-list-link"
+              onClick={handleLinkClick}
+            >
+              <div className="navigation-list-item">
+                <img
+                  src="images/info-icon.svg"
+                  className="navigation-list-link-image"
+                />
+              </div>
+            </a>
+
+            <a
+              href="#projects-section"
+              role="menuitem"
+              className="navigation-list-link"
+              onClick={handleLinkClick}
+            >
+              <div className="navigation-list-item">
+                <img
+                  src="images/skills-icon.svg"
+                  className="navigation-list-link-image"
+                />
+              </div>
+            </a>
+
+            <a
+              href="#projects-section"
+              role="menuitem"
+              className="navigation-list-link"
+              onClick={handleLinkClick}
+            >
+              <div className="navigation-list-item">
+                <img
+                  src="images/portfolio-icon.svg"
+                  className="navigation-list-link-image"
+                />
+              </div>
+            </a>
+
+            <a
+              href="#contact-section"
+              role="menuitem"
+              className="navigation-list-link"
+              onClick={handleLinkClick}
+            >
+              <div className="navigation-list-item">
+                <img
+                  src="images/work-history-icon.svg"
+                  className="navigation-list-link-image"
+                />
+              </div>
+            </a>
+          </div>
+          {/* <ul role="menubar" className="navigation-list">
             <li role="presentation" className="navigation-list-item">
               <a
                 href="#home-section"
                 role="menuitem"
                 className="navigation-list-link"
                 onClick={handleLinkClick}
-              >
-                Home
+              > 
+                <img src="images/home-icon.png" className='navigation-list-link-image' />
               </a>
             </li>
             <li role="presentation" className="navigation-list-item">
@@ -52,7 +130,7 @@ function NavBar() {
                 className="navigation-list-link"
                 onClick={handleLinkClick}
               >
-                About Me
+                About
               </a>
             </li>
             <li role="presentation" className="navigation-list-item">
@@ -75,7 +153,7 @@ function NavBar() {
                 Contact
               </a>
             </li>
-          </ul>
+          </ul> */}
         </div>
       </div>
     </div>
