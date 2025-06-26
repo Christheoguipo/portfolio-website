f1b9ec8ac5d4615d196472c750bf2cdb2c599fa6
Christheo Guipo <christheo.guipo@gmail.com>
2025-06-26 23:03:14 +0200

    feat: change home styling.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,59 +1,31 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Home.css";
 
 import PropTypes from "prop-types";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons/faLinkedinIn";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons/faEnvelope";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons/faWhatsapp";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function Home({ homeRef }) {
   return (
     <div ref={homeRef} id="home-section">
       <div className="profile">
-        <div className="box name-box">
-          <img src="images/multi-colored-bg.png" className="bg-name-box" />
-          {/* <img src="images/bg-sky.png" className="bg-name-box" /> */}
-          {/* <img src="images/bg-yellow.jpg" className="bg-name-box" />
-          <img src="images/red-black-bg.png" className="bg-name-box" /> */}
-          {/* <img src="images/bg-gradient.jpg" className="bg-name-box" /> */}
-          <div className="info">
-            <span className="meta">Hello, I am</span>
-            <span className="name">CHRISTHEO GUIPO</span>
-            <span className="meta">Software Developer</span>
-          </div>
-        </div>
-        <div className="box profile-box">
-          <img
-            src="images/profile-portrait.jpg"
-            className="image"
-            alt="A photograph of me"
-          />
-        </div>
-        <div className="box about-box">
-          <div className="carousel">
-            <div className="carousel-item">
-              {/* <img src="images/laptop.jpg" className="carousel-item-img" /> */}
-              <img src="images/bg-yellow.jpg" className="carousel-item-img" />
-              {/* <img src="images/multi-colored-bg.png" className="carousel-item-img" /> */}
-              {/* <div className="cover"></div> */}
-              <div className="carousel-item-content-container">
-                <span className="carousel-item-content">5+</span>
-              </div>
-            </div>
-          </div>
-          <div className="about-box-content-container">
-            {/* <img src="images/bg-sky.png" className="bg-sky" /> */}
-            <div className="about-box-content">
-              <span className="about">
-                I'm a passionate software developer with a focus on building
-                clean, efficient and scalable code.
-              </span>
-              <div className="icon-container">
-                <img className="icons" src="images/github-brands.svg" />
-                <img className="icons" src="images/linkedin-white.png" />
-                <img className="icons" src="images/gmail-white.png" />
-              </div>
-            </div>
+        <div className="profile-image-cover"></div>
+        {/* <img src="images/profile-portrait.jpg" className="profile-image" /> */}
+        <img src="images/theo-landscape.webp" className="profile-image" />
+        <div className="info">
+          <span className="meta">Hello, I am</span>
+          <span className="name">CHRISTHEO <span>GUIPO</span></span>
+          <span className="meta">Software Developer</span>
+          <div className="icon-container">
+            <FontAwesomeIcon className="icons" icon={faGithub} />
+            <FontAwesomeIcon className="icons" icon={faLinkedinIn} />
+            <FontAwesomeIcon className="icons" icon={faEnvelope} />
+            <FontAwesomeIcon className="icons" icon={faWhatsapp} />
           </div>
         </div>
       </div>
-      <div className="spacer"></div>
     </div>
   );
 }
