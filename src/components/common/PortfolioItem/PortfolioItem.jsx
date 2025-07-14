acd16027d261f366766a8f2ac51d58e1a575b9e5
Theo <christheo.guipo@gmail.com>
2025-07-14 08:11:52 +0200

    feat: add PortfolioItem Component.

---
+++ src/components/common/PortfolioItem/PortfolioItem.jsx
@@ -0,0 +1,60 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PropTypes from "prop-types";
+import "./PortfolioItem.css";
+
+function PortfolioItem({ data }) {
+  return (
+    <div className="portfolio">
+      <div className="portfolio-content">
+        <div className="portfolio-image-container">
+          {/* <FontAwesomeIcon className="portfolio-image" icon={faChartLine} /> */}
+          <img className="portfolio-image" src={data.img} />
+          {/* <FontAwesomeIcon className="portfolio-image" icon={faChartLine} /> */}
+        </div>
+        <div className="portfolio-info">
+          <span className="block text-2xl">{data.title}</span>
+
+          <p className="block content-center text-left text-sm pt-1">
+            {data.description}
+          </p>
+          <div>
+            <div className="portfolio-button-container">
+              {data.buttons.length > 0 ? (
+                <>
+                  <span className="text-xs">Links:</span>
+                  {data.buttons.map((button, index) => (
+                    <FontAwesomeIcon
+                      key={index}
+                      className="portfolio-button"
+                      icon={button}
+                    />
+                  ))}
+                </>
+              ) : (
+                <span className="text-xs">(This app is private)</span>
+              )}
+            </div>
+            {data.skills.length > 0 &&
+              data.skills.map((skill, index) => (
+                <div key={index} className="tech-skill">
+                  {skill}
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+PortfolioItem.propTypes = {
+  data: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    img: PropTypes.string,
+    buttons: PropTypes.array,
+    description: PropTypes.string,
+    skills: PropTypes.arrayOf(PropTypes.string),
+  }).isRequired,
+};
+
+export default PortfolioItem;
