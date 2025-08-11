60d9c4a4f89a3fc143edd7cade0f63f8ae2c81ad
Christheo Guipo <christheo.guipo@gmail.com>
2025-08-11 13:02:46 +0200

    feat: add copyright to footer.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,11 +2,13 @@ import Logo from '../common/Logo/Logo';
 import ContactIcons from '../common/ContactIcons/ContactIcons';
 
 function Footer() {
+    const year = new Date().getFullYear();
     return (
         <section className='flex flex-col py-20 mb-20 items-center'>
             <Logo size='mdNoMargin' />
             <span className='text-2xl md:text-3xl mt-4' >Let's connect!</span>
             <ContactIcons />
+            <span className='text-sm pt-2'>© Christheo Guipo {year}</span>
         </section>
     );
 }
